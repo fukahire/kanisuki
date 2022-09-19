dfa33acf3f72efd93601cbd5829ae70a6ed597f7
fukahire <hoy1589@gmail.com>
2022-09-19 22:39:33 +0800

    help
    i don't want to work

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html','./award.html','./news.html','./rule.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "'Noto Sans TC', sans-serif",
+      },
+      colors: {
+        'mainred': '#FF4177',
+        'maingreen': '#BAEF41',
+        'subred': '#FF88AA',
+        'subgreen': '#D4F588',
+        'maingray': '#E7EBDD',
+        'subgray': '#F3F5EE',
+        'markred': '#FF1255',
+        'markgreen': '#A9EB11',
+      },
+    },
+  },
+  plugins: [],
+}
